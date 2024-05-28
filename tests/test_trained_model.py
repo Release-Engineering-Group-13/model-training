a7d10d7d7c66fa2fbb3f2afb10a2863e3194bba3
Vanessa Timmer <v.timmer@student.tudelft.nl>
2024-05-28 15:19:35 +0200

    added tests for prediction output

--- tests/test_trained_model.py
+++ tests/test_trained_model.py
@@ -4,47 +4,54 @@ from joblib import load
 import pytest
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import numpy as np
 from src.model.model_train import model_train
 from src.model.model_predict import model_predict
+from lib_ml import preprocess_dataset, preprocess_input
 
-#input_folder = "data/interim"
-#if not os.path.exists(input_folder):
-#    raise FileNotFoundError(f"Input folder '{input_folder}' is empty")
-char_index = load('../data/interim/char_index.joblib')
-#x = load('data/interim/x_data.joblib')
-#y = load('data/interim/y_data.joblib')
+char_index = load('data/interim/char_index.joblib')
 
 @pytest.fixture()
 def trained_model():
-    #input_folder = "data/interim"
-    #if not os.path.exists(input_folder):
-    #    raise FileNotFoundError(f"Input folder '{input_folder}' is empty")
-    trained_model = load('../data/interim/model.joblib')
+    trained_model = load('data/interim/model.joblib')
     yield trained_model
 
 @pytest.fixture()
-def x_test_data():
-    x_test = load('../data/interim/x_data.joblib')
+def x_data():
+    x_test = load('data/interim/x_data.joblib')
     yield x_test
 
 @pytest.fixture()
-def y_test_data():
-    y_test = load('../data/interim/y_data.joblib')
+def y_data():
+    y_test = load('data/interim/y_data.joblib')
     yield y_test
 
-def test_nondet_robustness(x_test_data, y_test_data, trained_model):
-    original_acc = trained_model.evaluate(x_test_data[2], y_test_data[2])[1]
-    #original_acc = model_predict(x_test_data[2], y_test_data[2], trained_model)[2] 
+@pytest.fixture()
+def predictions(trained_model, x_data):
+    y_pred = trained_model.predict(x_data[2], batch_size=1000)
+    yield y_pred
+
+#def test_nondet_robustness(x_data, y_data, trained_model):
+    #Tests whether the model is robust to different random seeds. 
+    """
+    original_acc = trained_model.evaluate(x_data[2], y_data[2])[1]
     print("original acc: ", original_acc)
     for seed in [1,2]:
-        model_variant, _ = model_train(x_test_data[0], y_test_data[0], x_test_data[1], y_test_data[1], char_index, seed)
+        model_variant, _ = model_train(x_data[0], y_data[0], x_data[1], y_data[1], char_index, seed)
         print("Done training variant")
-        variant_acc = model_variant.evaluate(x_test_data[2], y_test_data[2])[1]
-        #variant_acc = model_predict(x_test_data, y_test_data, model_variant)[2] 
+        variant_acc = model_variant.evaluate(x_data[2], y_data[2])[1]
         print("variant acc: ", variant_acc)
         print(abs(original_acc - variant_acc))
-        assert abs(original_acc - variant_acc) <= 0.03
+        assert abs(original_acc - variant_acc) <= 0.03"""
+
 
 # Code to test preprocessing (features and data test)
 # Test on code slice (model development)
 # Code to test if predictions are between 0 and 1 (or phishing and legitimate, technically features and data i think??), and if they're not NaN (monitoring tests)
+def test_prediction_values_validity(x_data, trained_model, predictions):
+    #y_pred = trained_model.predict(x_data[2], batch_size=1000)
+    assert np.nan not in predictions and np.inf not in predictions
+
+def test_prediction_values_range(x_data, trained_model, predictions):
+    #y_pred = trained_model.predict(x_data[2], batch_size=1000)
+    assert np.all((predictions >= 0)|(predictions <= 1 ))
