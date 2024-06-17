d7227649d63d876fe6fe54987ce7751956632d04
Nickdubbel <nick.dubbeldam@live.nl>
2024-06-17 20:20:48 +0200

    Fix flake8 issues

--- update_readme.py
+++ update_readme.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 
+
 def parse_test_results(xml_file):
+
     tree = ET.parse(xml_file)
     root = tree.getroot()
     testsuite = root.find('testsuite')
@@ -10,27 +12,29 @@ def parse_test_results(xml_file):
     skipped = int(testsuite.attrib['skipped'])
     return tests, errors, failures, skipped
 
+
 def update_readme_text(tests, errors, failures, skipped, readme_file):
     with open(readme_file, 'r') as file:
         readme_content = file.readlines()
-    
+
     start_marker = '<!-- pytest-results-start -->'
     end_marker = '<!-- pytest-results-end -->'
-    
+
     start_index = readme_content.index(start_marker + '\n')
     end_index = readme_content.index(end_marker + '\n')
-    
+
     new_content = readme_content[:start_index+1]
-    new_content.append(f'\n### Pytest Results\n\n')
+    new_content.append('\n### Pytest Results\n\n')
     new_content.append(f'- **Tests:** {tests}\n')
     new_content.append(f'- **Errors:** {errors}\n')
     new_content.append(f'- **Failures:** {failures}\n')
     new_content.append(f'- **Skipped:** {skipped}\n')
-    new_content.append(f'\n')
+    new_content.append('\n')
     new_content += readme_content[end_index:]
-    
+
     with open(readme_file, 'w') as file:
         file.writelines(new_content)
+
 
 def generate_badge_url(tests, errors, failures, skipped):
     if errors > 0 or failures > 0:
@@ -46,22 +50,24 @@ def generate_badge_url(tests, errors, failures, skipped):
     badge_url = f"https://img.shields.io/badge/tests-{status}-{color}?logo=pytest"
     return badge_url
 
+
 def update_readme_badge(badge_url, readme_file):
     with open(readme_file, 'r') as file:
         readme_content = file.readlines()
-    
+
     start_marker = '<!-- pytest-badge-start -->'
     end_marker = '<!-- pytest-badge-end -->'
-    
+
     start_index = readme_content.index(start_marker + '\n')
     end_index = readme_content.index(end_marker + '\n')
-    
+
     new_content = readme_content[:start_index+1]
     new_content.append(f'![Pytest]({badge_url})\n')
     new_content += readme_content[end_index:]
-    
+
     with open(readme_file, 'w') as file:
         file.writelines(new_content)
+
 
 if __name__ == '__main__':
     xml_report = 'report.xml'
